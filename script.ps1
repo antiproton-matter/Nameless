e46a05b8cf8104a3f1730e0e2700eb5c68e19b12
antiproton-matter <166539466+antiproton-matter@users.noreply.github.com>
2025-03-04 21:20:15 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,3 +2,5 @@ Hostname
 Ipconfig /all
 Whoami /all
 wmic path softwareLicensingService get OA3xOriginalProductKey
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.11.0-1.msi -OutFile $env:tmp\wazuh-agent; msiexec.exe /i $env:tmp\wazuh-agent /q WAZUH_MANAGER='subdomain.25plrp.cz' WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME='Test_Doma' 
+NET START WazuhSvc
